09b1428a887106438ea6133462186d38208b9c50
duarte2025 <william.2025@hotmail.com>
2019-02-16 17:26:09 -0500

    Updates

---
+++ precache-manifest.2b0fb89a492d05a4a820cc905996f6a8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-gh-pages/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "cde22863ffca018955eb",
+    "url": "/react-gh-pages/static/js/runtime~main.cde22863.js"
+  },
+  {
+    "revision": "0354bf8c048b0831f909",
+    "url": "/react-gh-pages/static/js/main.0354bf8c.chunk.js"
+  },
+  {
+    "revision": "ba1ca5ba3f9c1e3c91ad",
+    "url": "/react-gh-pages/static/js/2.ba1ca5ba.chunk.js"
+  },
+  {
+    "revision": "0354bf8c048b0831f909",
+    "url": "/react-gh-pages/static/css/main.ccfaf5af.chunk.css"
+  },
+  {
+    "revision": "501ce915e64e0f3e0052b7c361c04ecc",
+    "url": "/react-gh-pages/index.html"
+  }
+];
